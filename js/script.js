4f74fbb0a2ec95c405d44d3f18b410f3660119ac
Treysen-Zobell <36913811+Treysen-Zobell@users.noreply.github.com>
2023-08-11 17:58:57 -0600

    Added top bar items and selectable sidebar items.
    Top bar items are currently unconnected placeholders and sidebar now supports expansion and collapse with animations. Sidebar items are now selectable but they do not change what shows in the tile area yet.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+const sidebar = document.querySelector("#sidebar");
+const tileArea = document.querySelector(".tile-area");
+const sidebarWidth = getComputedStyle(document.documentElement)
+    .getPropertyValue("--side-bar-width");
+const selectedRadio = document.querySelector('#sidebar')
+let sidebarExpanded = true;
+let platform = checkPlatform();
+
+
+function toggleSidebar() {
+    if (sidebarExpanded) {
+        sidebar.style.left = -sidebarWidth + "px";
+        tileArea.style.left = "0";
+    }
+    else {
+        sidebar.style.left = "0"
+        tileArea.style.left = sidebarWidth;
+    }
+    sidebarExpanded = !sidebarExpanded;
 }
+
+function checkPlatform() {
+    return (window.innerWidth < 600) ? "mobile" : "desktop";
+}
+
+function resizeCallback() {
+    let newPlatform = checkPlatform();
+    if (newPlatform === "mobile" && platform === "desktop" && sidebarExpanded) {
+        toggleSidebar();
+    }
+    platform = newPlatform;
+}
+
+
+window.onresize = resizeCallback;
